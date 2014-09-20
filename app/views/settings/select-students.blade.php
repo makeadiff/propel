f1cd4fde9ddf12f031836c01e750a26d6270eeae
Binny V A <binnyva@gmail.com>
2014-09-20 21:58:39 +0530

    Multiselect Filter enabled

--- app/views/settings/select-students.blade.php
+++ app/views/settings/select-students.blade.php
@@ -11,11 +11,12 @@
         <div class="row">
             <div class="col-md-offset-2 col-md-8">
             <form action="" method="post">
+            {{ Form::input('text', 'filter', '', array("style"=>"width:205px;", "placeholder"=>"Filter", 'id'=>'filter' )) }}<br />
             {{
                 Form::select('students[]',
                 $all_students, 
                 $selected_student_id,
-                array('multiple'=>true, 'size'=>10)); 
+                array('multiple'=>true, 'size'=>10, 'id'=>"students", "class"=>"multiselect", "style"=>"width:200px;" )); 
             }}<br />
             <input type="submit" name="action" class="btn btn-primary" value="Save" />
             </form>
@@ -27,4 +28,5 @@
     </div>
 </div>
 
+<script type="text/javascript" src="{{{URL::to('/')}}}/js/section/select-students.js"></script>
 @stop
