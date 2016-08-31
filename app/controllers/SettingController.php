c2c78f82d1cc6c8db168fea88ffafb8ce277ac53
Rohit Nair <rohitcse081@gmail.com>
2016-08-31 08:57:59 +0530

    Wingman & ASV Alumnis + Calendar

--- app/controllers/SettingController.php
+++ app/controllers/SettingController.php
@@ -52,7 +52,9 @@ class SettingController extends BaseController
         $selected_wingmen = Fellow::find($user_id)->wingman()->get();
 
         
-        $all_wingmen = DB::table('User as A')->join('City as B','A.city_id','=','B.id')->join('UserGroup as C','A.id','=','C.user_id')->select('A.id as id','A.name as name','A.phone as phone','C.group_id as group_id')->distinct()->where('B.id','=',$city_id)->wherein('C.group_id',[348,365])->where('A.status','=',1)->where('A.status','=','volunteer')->get();        
+        $all_wingmen = DB::table('User as A')->join('City as B','A.city_id','=','B.id')->join('UserGroup as C','A.id','=','C.user_id')->select('A.id as id','A.name as name','A.phone as phone','C.group_id as group_id')->distinct()->where('B.id','=',$city_id)->wherein('C.group_id',[348,365])->where('A.status','=',1)->where('A.user_type','=','volunteer')->get();        
+
+        //return $all_wingmen;        
 
         foreach ($all_wingmen as $wingman) {
             foreach ($selected_wingmen as $selected) {
@@ -71,7 +73,7 @@ class SettingController extends BaseController
             }                
         }            
 
-        return $all_wingmen;
+        //
 
         return View::make('settings/select-wingmen')->with('selected_wingmen',$selected_wingmen)->with('all_wingmen',$all_wingmen);
     }
