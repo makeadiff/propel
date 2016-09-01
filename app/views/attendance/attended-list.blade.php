00421f25f6312f2f5700ac08bc8c2e19e3e8c3c1
rhit99900 <rohitcse081@gmail.com>
2016-09-01 09:35:25 +0530

    Alumnus data issue fixed.

--- app/views/attendance/attended-list.blade.php
+++ app/views/attendance/attended-list.blade.php
@@ -27,19 +27,45 @@
                             $type = $entry->type;
                             if($type=='volunteer_time'){
                                 $variable = $entry->volunteerTime()->first();
+                                if(!empty($variable))
+                                {
+                                    $type = $entry->volunteerTime()->first()->volunteer()->first()->user_type; 
+                                    $status = $entry->volunteerTime()->first()->volunteer()->first()->status;
+                                }
                             }
                             elseif ($type=='wingman_time'){
                                 $variable = $entry->wingmanTime()->first();
+                                if(!empty($variable))
+                                {
+                                    $type = $entry->wingmanTime()->first()->wingman()->first()->user_type;
+                                    $status = $entry->wingmanTime()->first()->wingman()->first()->status;
+                                }
                             }                           
                         ?>
-                        @if(!empty($variable))
+                        @if(!empty($variable) && ($type=='volunteer' && $status=='1'))
                             <tr>
                             <td>{{date_format(date_create($entry->start_time),'l, jS F Y')}}</td>
                             <td>{{ ucwords(str_replace('_',' ',$entry->type)) }}</td>
                             @if($entry->type == "volunteer_time")
-                            <td>{{$entry->volunteerTime()->first()->volunteer()->first()->name}}</td>
+                            <td>
+                                <?php 
+                                    $type = $entry->volunteerTime()->first()->volunteer()->first()->user_type; 
+                                    $status = $entry->volunteerTime()->first()->volunteer()->first()->status;
+                                    $name = $entry->volunteerTime()->first()->volunteer()->first()->name;
+                                    if($type=='volunteer' && $status=='1')
+                                        echo $name;
+                                ?>
+                            </td>
                             @elseif($entry->type == "wingman_time")
-                            <td>{{$entry->wingmanTime()->first()->wingman()->first()->name}}</td>
+                            <td>
+                                <?php
+                                    $type = $entry->wingmanTime()->first()->wingman()->first()->user_type;
+                                    $status = $entry->wingmanTime()->first()->wingman()->first()->status;
+                                    $name = $entry->wingmanTime()->first()->wingman()->first()->name;
+                                    if($type=='volunteer' && $status=='1')
+                                        echo $name;
+                                ?>
+                            </td>
                             @endif
 
                             <td>
